d33c602483dca41fd4aec0e332be3dbf4c2b3dbd
KasareSimran <simrankasare5@gmail.com>
2026-01-15 18:18:10 +0530

    create the folder for grph/chart and added the file and wrte const data

---
+++ src/Components/Charts/chartConfig.js
@@ -0,0 +1,27 @@
+export const STATUS_OPTIONS = [
+  "totalDemands",
+  "openPositions",
+  "closedPositions",
+  "onHold",
+  "abandoned",
+  "onboardingProgress",
+  "profileShared",
+  "rejected",
+  "softSelect",
+  "candidateResigned",
+  "duplicateDemand",
+];
+ 
+export const STATUS_LABELS = {
+  totalDemands: "Total Demand",
+  openPositions: "Open Position",
+  closedPositions: "Closed Position",
+  onHold: "On Hold",
+  abandoned: "Abandoned",
+  onboardingProgress: "Onboarding In Progress",
+  profileShared: "Profile Shared",
+  rejected: "Rejected",
+  softSelect: "Soft Selected",
+  candidateResigned: "Candidate Resigned",
+  duplicateDemand: "Duplicate Demand",
+};
