528e31a40f34ed4ecc5ca6fec7f47f20b736b706
KasareSimran <simrankasare5@gmail.com>
2026-01-18 16:36:55 +0530

    update  the data

--- src/Components/Charts/chartConfig.js
+++ src/Components/Charts/chartConfig.js
@@ -1,15 +1,8 @@
 export const STATUS_OPTIONS = [
-  { key: "totalDemand", label: "Total Demand" },
+  // { key: "totalDemand", label: "Total Demand" },
   { key: "openPosition", label: "Open Position" },
   { key: "closedPosition", label: "Closed Position" },
-  { key: "onHold", label: "On Hold" },
-  { key: "abandoned", label: "Abandoned" },
-  { key: "onboardingProgress", label: "Onboarding In Progress" },
-  { key: "profileShared", label: "Profile Shared" },
   { key: "rejected", label: "Rejected" },
-  { key: "softSelect", label: "Soft Select" },
-  { key: "candidateResigned", label: "Candidate Resigned" },
-  { key: "duplicateDemand", label: "Duplicate Demand" },
 ];
  
 export const TIMELINE_OPTIONS = [
