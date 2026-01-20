52bb76a9e9f9fc8caa187e361673bc68b2a0acbd
KasareSimran <simrankasare5@gmail.com>
2026-01-20 11:21:17 +0530

    updated

--- src/Components/AuthProvider.jsx
+++ src/Components/AuthProvider.jsx
@@ -6,16 +6,7 @@ const AuthContext=createContext();
 export default function AuthProvider({children}){
     const [isAuthenticated,setIsAuthenticated]=useState(false);
 
-    //auto login on refresh if token exists
-    // useEffect(()=>{
-    //     const token =localStorage.getItem("token")
-    //     if(token && token !=="undefined" && token !==""){
-    //         setIsAuthenticated(true);
-    //     }else{
-    //         setIsAuthenticated(false);
-    //     }
-    // },[]);
-
+    
     return(
         <AuthContext.Provider 
         value={{isAuthenticated,setIsAuthenticated}}
