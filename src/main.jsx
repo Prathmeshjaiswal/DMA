116f68a0a15accce45bc4aa3963ff6325222fea9
KasareSimran <simrankasare5@gmail.com>
2026-02-03 01:19:02 +0530

    add permission providwer

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './Components/Auth/AuthProvider.jsx'
+import { PermissionProvider } from "./Components/Auth/PermissionProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <App />
+      <PermissionProvider>
+        <App />
+      </PermissionProvider>
     </AuthProvider>
   </StrictMode>,
 )
