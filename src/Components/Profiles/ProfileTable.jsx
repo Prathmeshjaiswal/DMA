b41db58b0ab1b14518c38e777fdd62cf4a2d0c9f
KasareSimran <simrankasare5@gmail.com>
2026-02-12 11:09:14 +0530

    update pagination

--- src/Components/Profiles/ProfileTable.jsx
+++ src/Components/Profiles/ProfileTable.jsx
@@ -825,7 +825,7 @@ export default function ProfileTable({
       pageSize: serverSize,
       total: serverTotal,
       showSizeChanger: true,
-      pageSizeOptions: ["5", "10", "20", "50"],
+      pageSizeOptions: ["10", "20", "50"],
       showTotal: (total) => `${total} profiles`,
       placement: "bottomRight",
       size: "default",
